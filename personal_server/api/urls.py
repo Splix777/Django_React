aab1c318c4ccfc42470cab8150f2e48697e7c956
splix <federicosalalois@gmail.com>
2024-02-13 17:03:05 +0100

    end of tutorial 10

--- personal_server/api/urls.py
+++ personal_server/api/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
-    path('user-in-room', UserInRoom.as_view())
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
 ]
 
 
